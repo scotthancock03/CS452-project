7dc3ae15da57b218feccafb9319c38999ab63f2a
Lath Nassar <123274085+NassarLaith@users.noreply.github.com>
2026-09-09 21:29:34 +0000

    Fixed App.js imports & routing and improved UI display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,8 @@
 import './App.css';
-<<<<<<< HEAD
-import Inventory from './components/inventory/Inventory';
-import AboutUs from './components/aboutus/AboutUs';
-import NavBar from './components/navbar/NavBar';
-=======
 
-import AboutUs from './components/AboutUs';
-import NavBar from './components/NavBar';
->>>>>>> 8a31d42 (Addition of files for the inventory component page: Inventory.jsx & Inventory.styles.js)
+import AboutUs from './components/aboutus/AboutUs';
+import NavBar from './components/navbar/NavBar'
+
 import Transactions from './components/transactions/Transactions';
 import Inventory from './components/inventory/Inventory';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
